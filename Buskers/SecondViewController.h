ac5d9023cb183db590d51facc71a4ef08f886584
MoncefB <moncef.biaz@polytechnique.edu>
2015-02-02 18:39:58 +0100

    First commit

---
+++ Buskers/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Buskers
+//
+//  Created by Moncef Biaz on 02/02/2015.
+//  Copyright (c) 2015 Moncef Biaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ Buskers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Buskers
+//
+//  Created by Moncef Biaz on 02/02/2015.
+//  Copyright (c) 2015 Moncef Biaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
